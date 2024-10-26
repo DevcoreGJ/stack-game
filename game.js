e801f62860edb6f107a64afa32007e20b0129915
DevcoreGJ <elliotledson@gmail.com>
2024-10-26 16:53:17 +0100

    Initial commit

---
+++ game.js
@@ -0,0 +1,50 @@
+const canvas = document.getElementById('gameCanvas');
+const context = canvas.getContext('2d');
+
+let stack = [];
+let size = 200;
+let pos = 0;
+let speed = 2;
+let direction = 1;
+
+function init() {
+    resizeCanvas();
+    stack.push({ size: size, pos: pos });
+    gameLoop();
+}
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    stack.forEach(block => {
+        block.pos = (canvas.width - block.size) / 2;
+    });
+}
+
+function gameLoop() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    stack.forEach((block, index) => {
+        context.fillRect(block.pos, canvas.height - (index + 1) * 30, block.size, 30);
+    });
+
+    let currentBlock = stack[stack.length - 1];
+    currentBlock.pos += speed * direction;
+    if (currentBlock.pos + currentBlock.size > canvas.width || currentBlock.pos < 0) {
+        direction *= -1;
+    }
+
+    requestAnimationFrame(gameLoop);
+}
+
+function addBlock() {
+    let lastBlock = stack[stack.length - 1];
+    let newBlock = { size: lastBlock.size, pos: lastBlock.pos };
+    stack.push(newBlock);
+}
+
+window.addEventListener('click', addBlock);
+window.addEventListener('touchstart', addBlock);
+window.addEventListener('resize', resizeCanvas);
+
+init();
